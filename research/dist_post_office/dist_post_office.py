28daed3db53c99e0c53e52399fdd5e0034425210
real <real.flayer@outlook.com>
2014-12-16 14:03:25 +0200

    Some fixes + Added permutation to distribute load equally

--- research/dist_post_office/dist_post_office.py
+++ research/dist_post_office/dist_post_office.py
@@ -6,12 +6,13 @@ Checking load in the Distributed Post Office.
 """
 
 import math
+import struct
 import random
-
 import collections
 
-import networkx as nx
+import hashlib
 
+import networkx as nx
 
 def hashi(i,x):
     """
@@ -53,14 +54,14 @@ class Node():
 
         # Initialize specials:
         # ( self.specials[hash_func_index][distance] )
+        self.specials = []
         for hi in range(self.dpo.num_hashes):
-            self.specials[hi] = [None for j in range(self.dpo.max_distance)]
-
+            self.specials.append([None for j in range(self.dpo.max_distance+1)])
 
         # Set my own index as the best at all hash functions for distance 0,
         # As I am the only node of distance 0 from myself.
         for hi in range(self.dpo.num_hashes):
-            self.add_node(self.ind,0,hi)
+            self.specials[hi][0] = self.ind
 
 
     def add_node(self,ind,distance,hash_i):
@@ -69,21 +70,18 @@ class Node():
         than the current node at self.specials[hash_i][distance].
         """
 
-        ident = self.dpo.nodes[ind].ident
-
         # If there was not yet a candidate for distance "distance" and hash
         # index hash_i, we choose this node.
         if self.specials[hash_i][distance] is None:
-            self.specials[hash_i][distance] = ident
+            self.specials[hash_i][distance] = ind
             return
 
         # If there is something in self.specials[hash_i][distance], we choose
         # the "higher" out of the two:
         cur_ind = self.specials[hash_i][distance]
-        cur_ident = self.nodes[cur_ind].ident
 
-        self.specials[hash_i][distance] = max([cur,ident],key=lambda x:\
-                hashi(hash_i,x))
+        self.specials[hash_i][distance] = max([cur_ind,ind],key=lambda j:\
+                hashi(hash_i,self.dpo.nodes[j]))
 
 
     def set_neighbours(self,nei_nodes):
@@ -107,7 +105,7 @@ class Node():
         self.specials_dc = {}
 
         for hi in range(self.dpo.num_hashes):
-            for dist in range(self.dpo.max_distance):
+            for dist in range(self.dpo.max_distance+1):
                 ind = self.specials[hi][dist]
                 # All the specials should be filled by nodes from the network
                 assert ind is not None
@@ -146,8 +144,6 @@ class DPostOffice():
         self.gen_nodes()
         self.install_neighbours()
 
-        # Calculate specials for every node in the network:
-        self.calc_specials()
 
     def rand_ident(self):
         """
@@ -220,7 +216,7 @@ class DPostOffice():
         assert self.max_distance >= 1
 
         # Iterate over all distances, starting from 0:
-        for dist in range(self.max_distance-1):
+        for dist in range(self.max_distance):
             # Iterate over all nodes:
             for nd in self.nodes:
                 # Iterate over all hash functions:
@@ -231,12 +227,12 @@ class DPostOffice():
                         # function according to information from it's
                         # neighbour nei_ind:
                         nei = self.nodes[nei_ind]
-                        nd.add_node(nei.specials[hash_i][dist],dist+1,hi)
+                        nd.add_node(nei.specials[hi][dist],dist+1,hi)
 
                     # Get special from nd's previous distance:
                     # We do this because the highest node of distance 5 might
                     # be also the highest node of distance 6, for example.
-                    nd.add_node(nd.specials[hash_i][dist],dist+1,hi)
+                    nd.add_node(nd.specials[hi][dist],dist+1,hi)
 
 
         # Install the specials_dc dictionary. This structure will be useful for
@@ -259,20 +255,29 @@ class DPostOffice():
 
         # intersection of known specials of a and b.
         # This gives us all the possible mediators between a and b:
-        mediators = set(sa.keys()).intersection(set(sb.keys()))
+        mediators = list(set(sa.keys()).intersection(set(sb.keys())))
         
         # The set of mediators is expected to be nonempty, as the "highest" of
         # every hash function should be both inside sa and sb:
         assert len(mediators) > 0
 
-        def path_dist(med):
+        # Create a random permutation of the same size of mediators.
+        # This is done to randomize the choice of mediator, in case there are a
+        # few mediators of the same quality.
+        rperm = list(range(len(mediators)))
+        random.shuffle(rperm)
+
+        def path_dist(i):
             """
             Length of path obtained by mediator.
             """
-            return sa[med] + sb[med]
+            return (sa[mediators[i]] + sb[mediators[i]],\
+                    rperm[i])
 
         # Return some mediator that gives shortest path:
-        return min(mediators,key=path_dist)
+        med_ind = min(range(len(mediators)),key=path_dist)
+        return mediators[med_ind]
+        # return min(mediators,key=path_dist)
 
     def measure_load(self,tries):
         """
@@ -310,27 +315,50 @@ def gen_gnp_graph(i):
 
 
 def go():
-    i = 11      # Parameter for graph generation.
+    ########[Parameters]########################
+    i = 12      # Parameter for graph generation.
     ident_bits = math.ceil(i*2.6)
-    num_hashes = 5
+    num_hashes = 1
+
+    # Number of most common mediators to show:
+    num_most_common = 0x10
+    # Number of messages to simulate delivery:
+    num_mess = 0x10000
+    ############################################
+
     print("||| i =",i)
     print("||| num_hashes =",num_hashes)
     print("||| ident_bits =",ident_bits)
 
     print("Generating graph...")
-    # g = gen_grid_graph(i)
-    g = gen_gnp_graph(i)
-    print("Generating Network and calculating specials ...")
+    g = gen_grid_graph(i)
+    # g = gen_gnp_graph(i)
+
+    print("Generating Network...")
     dp = DPostOffice(graph=g,\
             num_hashes=num_hashes,\
             ident_bits=ident_bits)
 
-    print("Simulating messages...")
-    cnt_med = dp.measure_load(0x1000)
+    print("Calculating specials...")
+    # Calculate specials for every node in the network:
+    dp.calc_specials()
 
-    print("most common mediators: ")
-    for cm in cnt_med.most_common(num_hashes * 2):
-        print(cm)
+    print("Simulating" ,num_mess, "messages delivery...")
+    cnt_med = dp.measure_load(num_mess)
+
+    print("\nmost common mediators:\n")
+    # Print header:
+    header_ln = " {med:15s} | {ratio:10s} | {times:15s} ".format(\
+            med="mediator index",ratio="ratio",\
+            times="messages routed")
+    print(header_ln)
+    print("-" * len(header_ln))
+
+    # Print table lines:
+    for med,times in cnt_med.most_common(num_most_common):
+        ln = " {med:15d} | {ratio:10f} | {times:15d} ".format(\
+                med=med,ratio=times/num_mess,times=times)
+        print(ln)
 
 
 

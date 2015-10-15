fe42e7faad96d5edd09ae8bb39392749f4fe71cd
real <real.flayer@outlook.com>
2015-10-15 12:02:36 +0300

    Changed algorithm to work by sending update messages at time ticks, like in the paper.

--- vdht_cycles_rounds/alg_state.py
+++ vdht_cycles_rounds/alg_state.py
@@ -19,6 +19,14 @@ class Finger:
         self.r = None
 
 
+# A structure for one node:
+class Node:
+    def __init__(self):
+        self.fingers = None
+        # Temporary recipients for the next update message to send.
+        self.temp_recipients = set()
+
+
 # A state in the algorithm:
 class AlgState:
     def __init__(self,g):
@@ -47,21 +55,27 @@ class AlgState:
         # Calculate the highest possible l such that 2^{l-1} < len(g):
         self._l = int(math.log2(len(g) - 1)) + 1
 
+
+        # Build a dictionary of nodes:
+        self._nodes = {node:Node() for node in self._g}
+
         # Build fingers structure:
         self._fingers = {node:dict() for node in self._g}
         for node in self._g:
+            self._nodes[node].fingers = dict()
+
             # Adding 0 finger:
-            self._fingers[node][0] = Finger()
+            # self._nodes[node].fingers[0] = Finger()
 
             # Adding negative fingers:
             for j in range(self._l):
                 f = -(2**j)
-                self._fingers[node][f] = Finger()
+                self._nodes[node].fingers[f] = Finger()
 
             # Adding positive fingers:
             for j in range(self._l):
                 f = 2**j
-                self._fingers[node][f] = Finger()
+                self._nodes[node].fingers[f] = Finger()
                 
         res = self.alg_iter()
         if not res:
@@ -72,7 +86,85 @@ class AlgState:
         """
         Return the "next node": The right node of finger 0.
         """
-        return self._fingers[node][0].r
+        return self._nodes[node].fingers[1].r
+
+    def _process_update_msg(self,rnode,snode,uset):
+        """
+        Node rnode processes and update message with set uset, sent from snode
+        Returns True is anything has changed in rnode's fingers. Returns False
+        otherwise.
+        """
+
+        # Add snode to rnode's temp_recipients, if rnode doesn't know snode.
+        # This way rnode will send to snode a set of his known nodes in the
+        # next algorithm iteration.
+        if snode not in self._get_known(rnode):
+            self._nodes[rnode].temp_recipients.add(snode)
+
+
+        # Add snode to the set of nodes, to build an extended update set:
+        euset = set(uset)
+        euset.add(snode)
+
+        # Has anything changed in rnode's fingers?
+        changed = False
+
+        # Update left side:
+        for f in self._nodes[rnode].fingers:
+            loc = (rnode + f) % len(self._g)
+
+            bl = self._best_l(loc,euset)
+
+            cur_l = self._nodes[rnode].fingers[f].l
+            if cur_l is None:
+                self._nodes[rnode].fingers[f].l = bl
+            else:
+                if self._d(bl,rnode) < self._d(cur_l,rnode):
+                    assert cur_l != bl
+                    self._nodes[rnode].fingers[f].l = bl
+                    changed = True
+
+
+        # Update right side:
+        for f in self._nodes[rnode].fingers:
+            loc = (rnode + f) % len(self._g)
+
+            br = self._best_r(loc,euset)
+
+            cur_r = self._nodes[rnode].fingers[f].r
+            if cur_r is None:
+                self._nodes[rnode].fingers[f].r = br
+            else:
+                if self._d(rnode,br) < self._d(rnode,cur_r):
+                    assert cur_r != br
+                    self._nodes[rnode].fingers[f].r = br
+                    changed = True
+
+        return changed
+
+
+    def _time_tick(self,node):
+        """
+        A time tick has occured for node node.
+        node will send update messages to all his known nodes, and set the
+        temp_recipients set to be empty.
+
+        Returns if any node has changed his fingers state. True for change,
+        False for no change.
+        """
+        # Has any node changed his fingers state?
+        changed = False
+
+        known = self._get_known(node)
+        for rnode in known:
+            res = self._process_update_msg(rnode,node,known)
+            changed = changed or res
+
+        # Clear node's temp_recipients:
+        self._nodes[node].temp_recipients.clear()
+
+        return changed
+
 
     def alg_iter(self):
         """
@@ -84,33 +176,24 @@ class AlgState:
         changed = False
 
         for node in self._g:
-            # Get all known nodes to node:
-            known = self._get_known(node)
-
-            for f in self._fingers[node]:
-                # Absolute location of finger:
-                loc = (node + f) % len(self._g)
-
-                # Update left side:
-                bl = self._best_l(loc,known)
-                if not self._fingers[node][f].l == bl:
-                    self._fingers[node][f].l = bl
-                    changed = True
-
-                # Update right side:
-                br = self._best_r(loc,known)
-                if not self._fingers[node][f].r == br:
-                    self._fingers[node][f].r = br
-                    changed = True
+            res = self._time_tick(node)
+            changed = changed or res
 
         return changed
+
 
     def run_until_stat(self):
         """
         Iterate until a stationary state is acheived.
         """
-        while self.alg_iter() is True:
-            pass
+        changed = True
+        while True:
+            prev_changed = changed
+            changed = self.alg_iter()
+            # We only break if we have two consecutive iterations where a
+            # change didn't happen.
+            if (not changed) and (not prev_changed):
+                break
 
 
     def _get_known(self,node):
@@ -118,7 +201,7 @@ class AlgState:
         Get all known nodes to a node using:
         - His initial neighbors on the graph.
         - The nodes he maintains on his fingers.
-        - All the fingers and initial neighbors of his finger nodes.
+        - Temporary recipients
         """
 
         known = set()
@@ -126,29 +209,18 @@ class AlgState:
         known.update(self._g.neighbors(node))
 
         # Add fingers:
-        for fin in self._fingers[node].values():
+        for fin in self._nodes[node].fingers.values():
             if fin.l is not None:
                 known.add(fin.l)
             if fin.r is not None:
                 known.add(fin.r)
 
-        # For each finger node, add his fingers and graph neighbors:
-        for ofin in self._fingers[node].values():
-            for fnode in [ofin.l,ofin.r]:
-                if fnode is None:
-                    continue
-
-                # Add the neighbors of fnode:
-                known.update(self._g.neighbors(fnode))
-
-                for fin in self._fingers[fnode].values():
-                    if fin.l is not None:
-                        known.add(fin.l)
-                    if fin.r is not None:
-                        known.add(fin.r)
+        # Add temporary recipients:
+        known.update(self._nodes[node].temp_recipients)
 
         # Make sure that node is not included in the known set:
         known.discard(node)
+
         return known
 
     def _d(self,x,y):
@@ -242,6 +314,15 @@ class AlgState:
         return True
 
 
+    def is_only_one_cycle(self):
+        """
+        Check if there is just one cycle.
+        """
+        # Count the amount of cycle representatives. If it is 1, return True.
+        # Otherwise return False
+        return sum(1 for _ in self._iter_cycle_reps()) == 1
+
+
     def is_all_cycles_one_round(self):
         """
         Check if all the cycles are of one round.
@@ -261,7 +342,7 @@ def go():
     """
     Run the IterF algorithm on some random graphs.
     """
-    l = 9
+    l = 7
     n = 2**l
     p = 3*math.log(n)/n
 
@@ -274,8 +355,14 @@ def go():
         algs.run_until_stat()
 
         # Print the properties of the resulting state:
-        print('next_node Injective: {} , One round cycles:  {}'.format(\
-                algs.is_next_node_injection(),algs.is_all_cycles_one_round()))
+        print('next_node Injective: {} | '
+              'One round cycles: {} | '
+              'Is only one cycle: {}'.\
+                      format(\
+                algs.is_next_node_injection(),\
+                algs.is_all_cycles_one_round(),\
+                algs.is_only_one_cycle())\
+                )
 
 if __name__ == '__main__':
     go()

0b2c703b42ca4ba780e10c8aa339f3ae94d2a270
real <real@freedomlayer.org>
2016-12-17 21:25:19 +0200

    Reorganized code in modules.

--- net_coords/src/main.rs
+++ net_coords/src/main.rs
@@ -1,61 +1,69 @@
 extern crate rand;
-use std::collections::HashSet;
 
-use rand::Rng;
-use rand::distributions::{IndependentSample, Range};
-
-struct Network {
-    n: u32,
-    num_neighbours: u32,
-    neighbours: Vec<HashSet<u32>>,
-}
+mod network_sim {
+    use std::collections::HashSet;
+    use rand::Rng;
+    use rand::distributions::{IndependentSample, Range};
 
 
-impl Network {
-    fn new(n: u32, num_neighbours: u32) -> Self {
-        let mut net: Self = Network {
-            n: n, 
-            num_neighbours: num_neighbours,
-            neighbours: Vec::new()
-        };
-
-        for _ in 0 .. n {
-            net.neighbours.push(HashSet::new());
-        }
-
-        net
+    pub struct Network {
+        n: u32,
+        num_neighbours: u32,
+        neighbours: Vec<HashSet<u32>>,
     }
 
-    fn build_network<R: Rng>(&mut self, rng: &mut R) -> &mut Self {
-        let rand_range: Range<u32> = Range::new(0,self.n);
-        // Connect node v to about num_neighbours other nodes:
-        for v in 0 .. self.n {
-            for _ in 0 .. self.num_neighbours {
-                let u = rand_range.ind_sample(rng);
-                if u == v {
-                    // Avoid self loops
-                    continue
-                }
-                if self.neighbours[v as usize].contains(&u) {
-                    // Already has this edge.
-                    continue
-                }
-                // Add edge:
-                self.neighbours[v as usize].insert(u);
-                self.neighbours[u as usize].insert(v);
+
+    impl Network {
+        pub fn new(n: u32, num_neighbours: u32) -> Self {
+            let mut net: Self = Network {
+                n: n, 
+                num_neighbours: num_neighbours,
+                neighbours: Vec::new()
+            };
+
+            for _ in 0 .. n {
+                net.neighbours.push(HashSet::new());
             }
+
+            net
         }
-        self
+
+        pub fn build_network<R: Rng>(&mut self, rng: &mut R) -> &mut Self {
+            let rand_range: Range<u32> = Range::new(0,self.n);
+            // Connect node v to about num_neighbours other nodes:
+            for v in 0 .. self.n {
+                for _ in 0 .. self.num_neighbours {
+                    let u = rand_range.ind_sample(rng);
+                    if u == v {
+                        // Avoid self loops
+                        continue
+                    }
+                    if self.neighbours[v as usize].contains(&u) {
+                        // Already has this edge.
+                        continue
+                    }
+                    // Add edge:
+                    self.neighbours[v as usize].insert(u);
+                    self.neighbours[u as usize].insert(v);
+                }
+            }
+            self
+        }
     }
 }
 
 #[cfg(test)]
 mod test {
+    extern crate rand;
+    use rand::Rng;
+    use rand::distributions::{IndependentSample, Range};
+    use network_sim::Network;
+
     #[test]
     fn test_build_network() {
         let mut rng = rand::thread_rng();
-        let net = Network::new().
-            build_network(40,3,&mut rng);
+        let net = Network::new(40,3).
+            build_network(&mut rng);
     }
 }
 

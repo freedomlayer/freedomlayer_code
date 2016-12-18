74da32dd6480c48f2becb07435c1ca181212bbc2
real <real@freedomlayer.org>
2016-12-18 18:43:23 +0200

    Changed parameters and printing them in beginning of program.

--- net_coords/src/main.rs
+++ net_coords/src/main.rs
@@ -9,14 +9,20 @@ fn check_unique_coord() {
     let mut rng = rand::thread_rng();
     let l: u32 = 20;
     let n: usize = ((2 as u64).pow(l)) as usize;
-    let num_neighbours: usize = (l/2) as usize;
-    let num_landmarks: usize = (2*l) as usize;
+    let num_neighbours: usize = (1.5 * (n as f64).ln()) as usize;
+    let num_landmarks: usize = (l*l) as usize;
+    let num_iter_coords = (1.5 * ((l as f64) / (num_neighbours as f64).ln())) as u32;
+
+    println!("n = {}",n);
+    println!("num_neighbours = {}",num_neighbours);
+    println!("num_landmarks = {}",num_landmarks);
+    println!("num_iters_coords = {}",num_iter_coords);
 
     let mut net = Network::new()
         .build_network(n,num_neighbours,&mut rng)
         .choose_landmarks(num_landmarks,&mut rng);
 
-    for i in 0..7 {
+    for i in 0..num_iter_coords {
         net.iter_coords();
         println!("Iter number {}",i);
     }

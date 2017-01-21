b40372c20c7588292b5066f58eb58bc2d3589a6c
real <real.flayer@outlook.com>
2017-01-21 18:22:35 +0200

    Running check_approx_dist from main.

--- research/network_coords/net_coords/src/main.rs
+++ research/network_coords/net_coords/src/main.rs
@@ -5,12 +5,16 @@ mod random_util;
 mod statistic;
 mod checks;
 
+use coord_mappers::approx_max_dist;
+
 #[cfg(not(test))]
-use checks::check_unique_coord;
+use checks::{check_unique_coord, check_approx_dist};
 
 #[cfg(not(test))]
 fn main() {
-    // check_ring_nums();
-    check_unique_coord(16);
+    // check_ring_nums(16);
+    // check_unique_coord(16);
+    check_approx_dist(16, approx_max_dist);
+
 }
 

6dcc87a36b751f31de47ccdbd23088d4c419d1c6
real <real.flayer@outlook.com>
2017-01-21 20:48:16 +0200

    Checking for different network sizes.

--- research/network_coords/net_coords/src/main.rs
+++ research/network_coords/net_coords/src/main.rs
@@ -14,7 +14,10 @@ use checks::{check_unique_coord, check_approx_dist};
 fn main() {
     // check_ring_nums(16);
     // check_unique_coord(16);
+    check_approx_dist(14, approx_max_dist);
+    check_approx_dist(15, approx_max_dist);
     check_approx_dist(16, approx_max_dist);
+    check_approx_dist(17, approx_max_dist);
 
 }
 

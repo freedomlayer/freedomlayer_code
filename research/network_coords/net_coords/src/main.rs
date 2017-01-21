a476f5ab6a99d2dad8f934abd4cb9ae29ed1b592
real <real.flayer@outlook.com>
2017-01-21 21:53:52 +0200

    Changed checks called by main.

--- research/network_coords/net_coords/src/main.rs
+++ research/network_coords/net_coords/src/main.rs
@@ -5,7 +5,7 @@ mod random_util;
 mod statistic;
 mod checks;
 
-use coord_mappers::approx_max_dist;
+use coord_mappers::{approx_max_dist, approx_avg_dist};
 
 #[cfg(not(test))]
 use checks::{check_unique_coord, check_approx_dist};
@@ -14,10 +14,13 @@ use checks::{check_unique_coord, check_approx_dist};
 fn main() {
     // check_ring_nums(16);
     // check_unique_coord(16);
-    check_approx_dist(14, approx_max_dist);
-    check_approx_dist(15, approx_max_dist);
-    check_approx_dist(16, approx_max_dist);
-    check_approx_dist(17, approx_max_dist);
+    // check_approx_dist(14, approx_max_dist);
+    // check_approx_dist(15, approx_max_dist);
+    // check_approx_dist(16, approx_max_dist);
+    // check_approx_dist(17, approx_max_dist);
 
+
+    println!("approx_max_dist");
+    check_approx_dist(16,approx_avg_dist);
 }
 

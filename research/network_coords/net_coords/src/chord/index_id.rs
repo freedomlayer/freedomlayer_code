2b9b6b3f1f3cc60d6b0f6289addf2001dedac253
real <real@freedomlayer.org>
2017-05-13 10:04:55 +0300

    Added tests and solved bugs in IndexId.

--- research/network_coords/net_coords/src/chord/index_id.rs
+++ research/network_coords/net_coords/src/chord/index_id.rs
@@ -16,7 +16,7 @@ impl IndexId {
         }
     }
     pub fn index_to_id(&self,index: usize) -> Option<RingKey> {
-        match index > self.index_id.len() {
+        match index >= self.index_id.len() {
             true => None,
             _ => Some(self.index_id[index])
         }
@@ -25,9 +25,35 @@ impl IndexId {
         self.id_index.get(&id).map(|&index| index)
     }
 
-    pub fn add_node(&mut self, index: usize, id: RingKey) {
+    pub fn add_node(&mut self, id: RingKey) -> usize {
         let new_index:usize = self.index_id.len();
         self.index_id.push(id);
-        self.id_index.insert(id,index);
+        self.id_index.insert(id,new_index);
+        new_index
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_index_id() {
+        let mut index_id = IndexId::new();
+        index_id.add_node(2); // index: 0
+        index_id.add_node(3); // index: 1
+        assert!(index_id.index_to_id(0).unwrap() == 2);
+        assert!(index_id.index_to_id(1).unwrap() == 3);
+        assert!(index_id.index_to_id(2).is_none());
+        assert!(index_id.index_to_id(3).is_none());
+        assert!(index_id.index_to_id(4).is_none());
+
+        assert!(index_id.id_to_index(2).unwrap() == 0);
+        assert!(index_id.id_to_index(3).unwrap() == 1);
+        assert!(index_id.id_to_index(4).is_none());
+        assert!(index_id.id_to_index(0).is_none());
+    }
+}
+
+

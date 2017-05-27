2de8208d0babfe45aa69904ac75a39cd8a4ef751
real <real.flayer@outlook.com>
2017-05-27 15:15:55 +0300

    Cosmetics.

--- research/network_coords/net_coords/src/bin/weighted_routing.rs
+++ research/network_coords/net_coords/src/bin/weighted_routing.rs
@@ -99,21 +99,23 @@ pub fn check_weighted_routing(net: &Network<usize>, coords: &Vec<Vec<u64>>, land
 
 #[cfg(not(test))]
 fn main() {
+    println!("Landmarks routing in a random graph, using lookahead");
     for l in 11 .. 21 {
     // let l: u32 = 15;
         println!("--------------------------------");
-        let n: usize = ((2 as u64).pow(l)) as usize;
-        let num_neighbours: usize = (1.5 * (n as f64).ln()) as usize;
+        let num_nodes: usize = ((2 as u64).pow(l)) as usize;
+        let num_neighbours: usize = (1.5 * (num_nodes as f64).ln()) as usize;
         let num_landmarks: usize = (((l*l) as u32)/3) as usize;
 
-        println!("n = {}",n);
+        println!("l = {}",l);
+        println!("num_nodes = {}",num_nodes);
         println!("num_neighbours = {}",num_neighbours);
         println!("num_landmarks = {}",num_landmarks);
 
         let seed: &[_] = &[1,2,3,4,5];
         let mut rng: StdRng = rand::SeedableRng::from_seed(seed);
         println!("Creating the network...");
-        let net = random_net(n,num_neighbours,&mut rng);
+        let net = random_net(num_nodes, num_neighbours,&mut rng);
         let landmarks = choose_landmarks(&net,num_landmarks, &mut rng);
         println!("Iterating through coordinates");
         let coords = build_coords(&net, &landmarks);

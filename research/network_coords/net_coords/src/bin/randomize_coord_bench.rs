545335efcae4aee2e47836ed5dc69c019e54efa0
real <real@freedomlayer.org>
2017-06-28 14:33:34 +0300

    Looking at coordinates.

--- research/network_coords/net_coords/src/bin/randomize_coord_bench.rs
+++ research/network_coords/net_coords/src/bin/randomize_coord_bench.rs
@@ -7,8 +7,9 @@ use rand::{StdRng};
 // use std::hash::Hash;
 use net_coords::landmarks::coords::{build_coords, choose_landmarks};
 // use net_coords::landmarks::randomize_coord::randomize_coord_rw;
+use net_coords::landmarks::randomize_coord::randomize_coord_rw_mix;
 // use net_coords::landmarks::randomize_coord::randomize_coord_rw_sparse;
-use net_coords::landmarks::randomize_coord::randomize_coord_rw_directional;
+// use net_coords::landmarks::randomize_coord::randomize_coord_rw_directional;
 use net_coords::landmarks::randomize_coord::{calc_upper_constraints /*, is_rw_coord */};
 use net_coords::network_gen::{gen_network};
 
@@ -25,6 +26,7 @@ fn main() {
         0 => print!("rand    ; "),
         1 => print!("2d      ; "),
         2 => print!("rand+2d ; "),
+        3 => print!("planar  ; "),
         _ => unreachable!(),
     }
     // print!("nt={:1}; ",net_type);
@@ -46,6 +48,8 @@ fn main() {
     println!("Choosing landmarks...");
     let landmarks = choose_landmarks(&net, num_landmarks, &mut network_rng);
     println!("landmarks = {:?}", landmarks);
+    println!();
+    println!();
     println!("Building coordinates...");
     let coords = match build_coords(&net, &landmarks) {
         Some(coords) => coords,
@@ -55,14 +59,24 @@ fn main() {
 
     // Randomize a coordinate:
     for _ in 0 .. 1 {
-        let rcoord = randomize_coord_rw_directional(&upper_constraints, 
+        // let rcoord = randomize_coord_rw_directional(&upper_constraints, 
+        //                                        &landmarks, &coords, &mut network_rng);
+        let rcoord = randomize_coord_rw_mix(&upper_constraints, 
                                                &landmarks, &coords, &mut network_rng);
         println!("rcoord = {:?}", rcoord);
         // println!("i = {}",i);
     }
+    println!();
+    println!();
+    println!("Real coordinates:");
+    for i in 0 .. 5 {
+        println!("{:?}", coords[i]);
+        println!();
+    }
+
     // assert!(is_rw_coord(&coords[0], &upper_constraints, &landmarks, &coords));
-    println!("coords[0]");
-    println!("{:?}",coords[0]);
+    // println!("coords[0]");
+    // println!("{:?}",coords[0]);
 
     println!();
 }
